a48a8cb5989d001fde9ccf3d6047bda8407b813c
Dheeraj-Manwani <dkmanwani2000@gmail.com>
2025-06-22 20:05:41 +0530

    fixed build issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 "use server";
 
 import React from "react";
-// import Header from "@/components/Header";
-// import Sidebar from "@/components/Sidebar";
 import { SwipeableNewsFeed } from "@/components/news-feeds/SwipeableNewsFeed";
 import { getArticleBySlug } from "@/actions/articles";
-import { Article } from "@/db/schema/article";
-import Head from "next/head";
 import { ArticleType } from "@prisma/client/index-browser";
 import { getThumbnailFromYouTube } from "@/actions/misc";
 import { Metadata } from "next";
-// import { useSearchParams } from "next/navigation";
 
 export async function generateMetadata({
   searchParams,
@@ -47,57 +42,12 @@ export async function generateMetadata({
   };
 }
 
-const Home = ({
-  article,
-  articleSlug,
-}: {
-  article: Article | null;
-  articleSlug: string | null;
-}) => {
-  // const [selectedCategoryId, setSelectedCategoryId] = useState(1); // Default to "All" category
-  // const [sidebarOpen, setSidebarOpen] = useState(false);
+export default async function Home(props: {
+  searchParams: { article?: string };
+}) {
+  // const awaitedProps = await props;
+  console.log("searchParams ", (await props.searchParams).article);
+  const articleSlug = (await props.searchParams).article;
 
-  // const handleMenuClick = () => {
-  //   setSidebarOpen(true);
-  // };
-
-  // const handleCloseSidebar = () => {
-  //   setSidebarOpen(false);
-  // };
-  // const searchParams = useSearchParams();
-  // const articleSlug = searchParams.get("article");
-  // const imageUrl =
-  //   article?.type == ArticleType.VIDEO_N_TEXT
-  //     ? article?.imageUrls?.[0]
-  //     : article?.type == ArticleType.YOUTUBE
-  //     ? getThumbnailFromYouTube(article?.videoUrl)
-  //     : "https://your-default-image.jpg";
-  // const fullUrl = `https://gondia-shorts.vercel.app?article=${article?.slug}`;
-
-  return (
-    <>
-      {/* {article && (
-        <Head>
-          <title>{article.title}</title>
-          <meta property="og:title" content={article.title} />
-          <meta
-            property="og:description"
-            content={"Read this article on Gondia Shorts"}
-          />
-          <meta property="og:image" content={imageUrl} />
-          <meta property="og:url" content={fullUrl} />
-          <meta property="og:type" content="article" />
-          <meta name="twitter:card" content="summary_large_image" />
-        </Head>
-      )} */}
-
-      <SwipeableNewsFeed
-        categoryId={0}
-        articleSlug={articleSlug}
-        // session={session}
-      />
-    </>
-  );
-};
-
-export default Home;
+  return <SwipeableNewsFeed categoryId={0} articleSlug={articleSlug} />;
+}
